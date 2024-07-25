af250f494632c316558f507040dbdbf4020a9971
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-07-25 11:55:50 -0300

    ft: config tailwind and styles in general

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        background: 'var(--background, #020617)',
+        'neural-blue': 'var(--Neural-Blue, #00C1FA)',
+        'muted-foreground': 'var(--muted-foreground, #94A3B8)',
+        border: 'var(--border, #334155)',
       },
     },
   },
