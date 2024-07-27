de6f92ff3a5e9f45478a69eb9ce7e37cd0cd4373
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-07-27 17:58:04 -0300

    fix: adjust order of validation of content before manipulating it

--- src/utils/paramsUrlMarvel.ts
+++ src/utils/paramsUrlMarvel.ts
@@ -5,12 +5,12 @@ export default function paramsUrlMarvel() {
   const publicKey = process.env.NEXT_PUBLIC_MARVEL_PUBLIC_KEY as string;
   const privateKey = process.env.NEXT_PUBLIC_MARVEL_PRIVATE_KEY as string;
 
-  const hash = MD5(ts + privateKey + publicKey).toString();
-  const params = `?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-
   if (!publicKey || !privateKey) {
     throw new Error('Missing API keys');
   }
+
+  const hash = MD5(ts + privateKey + publicKey).toString();
+  const params = `?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
 
   return params;
 }
