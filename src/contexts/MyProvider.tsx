79759644bd2f3d6564c058b29c69b66c1d8deeed
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-09-12 16:06:01 -0300

    fix: change order of useStates and space file just to be pretty

--- src/contexts/MyProvider.tsx
+++ src/contexts/MyProvider.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import MyContext from './MyContext';
 import { MyProviderProps } from '@/interfaces/contextInterfaces';
 import { CharacterContentModified } from '@/interfaces/characterDetailsInterfaces';
@@ -7,9 +7,9 @@ import { CharactersDataModified } from '@/interfaces/charactersInterfaces';
 
 export default function MyProvider({ children }: MyProviderProps) {
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const [howManyCharacters, setHowManyCharacters] = useState<number>(0);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [allData, setAllData] = useState<CharactersDataModified[]>([]);
+  const [howManyCharacters, setHowManyCharacters] = useState<number>(0);
   const [characters, setCharacters] = useState<CharactersDataModified[]>([]);
   const [currentCharacter, setCurrentCharacter] = useState<CharacterContentModified>({
     id: 0,
@@ -26,16 +26,16 @@ export default function MyProvider({ children }: MyProviderProps) {
   const values = {
     searchTerm,
     setSearchTerm,
-    howManyCharacters,
-    setHowManyCharacters,
     currentPage,
     setCurrentPage,
     allData,
     setAllData,
-    currentCharacter,
-    setCurrentCharacter,
+    howManyCharacters,
+    setHowManyCharacters,
     characters,
     setCharacters,
+    currentCharacter,
+    setCurrentCharacter,
   };
 
   return <MyContext.Provider value={values}>{children}</MyContext.Provider>;
--- src/interfaces/contextInterfaces.ts
+++ src/interfaces/contextInterfaces.ts
@@ -5,16 +5,16 @@ import { CharactersDataModified } from './charactersInterfaces';
 export interface MyContextType {
   searchTerm: string;
   setSearchTerm: (term: string) => void;
-  howManyCharacters: number;
-  setHowManyCharacters: (term: number) => void;
   currentPage: number;
   setCurrentPage: (term: number) => void;
-  currentCharacter: CharacterContentModified;
-  setCurrentCharacter: (term: CharacterContentModified) => void;
   allData: CharactersDataModified[];
   setAllData: (term: CharactersDataModified[]) => void;
+  howManyCharacters: number;
+  setHowManyCharacters: (term: number) => void;
   characters: CharactersDataModified[];
   setCharacters: (term: CharactersDataModified[]) => void;
+  currentCharacter: CharacterContentModified;
+  setCurrentCharacter: (term: CharacterContentModified) => void;
 }
 
 export interface MyProviderProps {
